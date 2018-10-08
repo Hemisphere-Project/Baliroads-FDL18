077b500ee4aad67949341ef9aedeb91bfb24e190
Jeremie <jeremie@hemisphere-project.com>
2018-10-08 16:18:26 +0200

    Flotteuse blinker class

--- Flotteuses_esp8266/Flotteuses_esp8266.ino
+++ Flotteuses_esp8266/Flotteuses_esp8266.ino
@@ -13,7 +13,50 @@ long OffTimeMin = 100;
 long OffTimeMax = 200;
 
 long OnTime, OffTime;
-int ledState = LOW;
+// int ledState = LOW;
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+class Blinker
+{
+	int pwmPin;
+	long OnTime;
+	long OffTime;
+	unsigned long previousMillis;
+
+  // Constructor
+  public:
+  Blinker(int pin)
+  {
+  OnTime = random(OnTimeMin,OnTimeMax);
+  OffTime = random(OffTimeMin,OffTimeMax);
+	pwmPin = pin;
+	previousMillis = 0;
+  }
+
+  void Update()
+  {
+    unsigned long currentMillis = millis();
+    // OFF
+    if(currentMillis-previousMillis>OffTime){
+      pwm1.setPin(pwmPin, 4095);
+    }
+    // ON
+    if(currentMillis-previousMillis>OffTime+OnTime){
+      pwm1.setPin(pwmPin, 0);
+      // RELOOP
+      previousMillis = currentMillis;
+      OnTime = random(OnTimeMin,OnTimeMax);
+      OffTime = random(OffTimeMin,OffTimeMax);
+    }
+  }
+};
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
 
 
 
@@ -22,27 +65,34 @@ void setup() {
   pwm1.setPWMFreq(1000);
   Wire.setClock(400000);
 
-  OnTime = random(OnTimeMin,OnTimeMax);
-  OffTime = random(OffTimeMin,OffTimeMax);
-  previousMillis = 0;
+  // OnTime = random(OnTimeMin,OnTimeMax);
+  // OffTime = random(OffTimeMin,OffTimeMax);
+  // previousMillis = 0;
 
 }
 
+Blinker flotteur1(0);
+Blinker flotteur2(1);
+
 void loop() {
 
-  unsigned long currentMillis = millis();
-  // OFF
-  if(currentMillis-previousMillis>OffTime){
-    pwm1.setPin(1, 0);
-  }
-  // ON
-  if(currentMillis-previousMillis>OffTime+OnTime){
-    pwm1.setPin(1, 4095);
-    // RELOOP
-    previousMillis = currentMillis;
-    OnTime = random(OnTimeMin,OnTimeMax);
-    OffTime = random(OffTimeMin,OffTimeMax);
-  }
+
+  flotteur1.Update();
+  flotteur2.Update();
+
+  // unsigned long currentMillis = millis();
+  // // OFF
+  // if(currentMillis-previousMillis>OffTime){
+  //   pwm1.setPin(1, 0);
+  // }
+  // // ON
+  // if(currentMillis-previousMillis>OffTime+OnTime){
+  //   pwm1.setPin(1, 4095);
+  //   // RELOOP
+  //   previousMillis = currentMillis;
+  //   OnTime = random(OnTimeMin,OnTimeMax);
+  //   OffTime = random(OffTimeMin,OffTimeMax);
+  // }
 
 
   // unsigned long currentMillis = millis();
