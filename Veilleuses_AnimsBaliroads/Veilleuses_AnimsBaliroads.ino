826f66840721fa924d0900a7503461e32ac477ea
Jeremie <jeremie@hemisphere-project.com>
2018-10-10 17:25:57 +0200

    anims

---
+++ Veilleuses_AnimsBaliroads/Veilleuses_AnimsBaliroads.ino
@@ -0,0 +1,19 @@
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+int timeDot = 200;
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+
+
+void loop() {
+
+}
