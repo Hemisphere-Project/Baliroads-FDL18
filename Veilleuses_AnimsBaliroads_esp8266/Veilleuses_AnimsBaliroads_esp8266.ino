833c03a21e92cae958943fee67a7564a5244fe6e
Jeremie <jeremie@hemisphere-project.com>
2018-11-12 18:53:38 +0100

    Add DMX

--- Veilleuses_AnimsBaliroads_esp8266/Veilleuses_AnimsBaliroads_esp8266.ino
+++ Veilleuses_AnimsBaliroads_esp8266/Veilleuses_AnimsBaliroads_esp8266.ino
@@ -1,6 +1,7 @@
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <ESPDMX.h> // https://github.com/Rickgg/ESP-Dmx
 
 //NodeMCU 1.0 (ESP-12E Module)
 //Programmateur AVRISP MKII
@@ -8,6 +9,8 @@
 // PWM cards
 Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver();
 
+// DMX
+DMXESPSerial dmx;
 
 int numBaliroads = 16;
 
@@ -29,21 +32,37 @@ void baliroad_ON(int pwmPin){
   // if (id % 2) { pixels.setPixelColor(id, pixels.Color(5,5,5)); }
   // else pixels.setPixelColor(id, pixels.Color(5,0,0));
   pwm1.setPin(pwmPin, onValue);
-  //Serial.printf("ON: %i \n", pwmPin);
+  //Serial.printf("BALI ON: %i \n", pwmPin);
 }
 void baliroad_OFF(int pwmPin){
   // pixels.setPixelColor(id, pixels.Color(0,0,0));
   pwm1.setPin(pwmPin, 0);
-  //Serial.printf("OFF: %i \n", pwmPin);
+  //Serial.printf("BALI OFF: %i \n", pwmPin);
+}
+
+void spot_ON(){
+  dmx.write(1, 255);
+  dmx.update();
+  //Serial.printf("DMX ON: 1 \n");
+}
+void spot_OFF(){
+  dmx.write(1, 0);
+  dmx.update();
+  //Serial.printf("DMX OFF: 1 \n");
 }
 
 
 void setup() {
 
   //Serial.begin(115200);
+
+  // PWM Board
   pwm1.begin();
   pwm1.setPWMFreq(1000);
   Wire.setClock(400000);
+
+  // DMX
+  dmx.init(32); // initialization for first 32 addresses by default
 
   Tstart = millis();
 
@@ -79,6 +98,12 @@ void loop() {
     TlastOn = Tnow;
     indexOn ++;
     if(indexOn == numBaliroads){ indexOn=0; goOn = !goOn; }
+
+    // TEST DMX
+    if(goOn == true) spot_ON();
+    else spot_OFF();
   }
+
+
 
 }
